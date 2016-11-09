8d138d3513b4910fc9a41280e6db106ccfd6c431
devonhackley <devonhackley@gmail.com>
2016-11-09 15:56:01 -0800

    homepage is looking good

---
+++ homeslider.js
@@ -0,0 +1,24 @@
+'use strict';
+/* This was found on the W3 website, http://www.w3schools.com/w3css/w3css_slideshow.asp, I understand the mechanics behind the slideshow */
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName('mySlides');
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex - 1].style.display = 'block';
+}
