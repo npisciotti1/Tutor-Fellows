b443eedd9c9bf917624751cac01893531ed4ad1c
devonhackley <devonhackley@gmail.com>
2016-11-10 12:16:13 -0800

    tutor page should be looking good, changes some class and id names

--- homeslider.js
+++ homeslider.js
@@ -1,9 +1,23 @@
 'use strict';
 /* This was found on the W3 website, http://www.w3schools.com/w3css/w3css_slideshow.asp, I understand the mechanics behind the slideshow */
 
-var captions = [];
+var captions = ['Our Main Area', 'Student Workplace', 'Private Rooms', 'Whiteboarding', 'Hangout Area', 'Common Room'];
+var left = document.getElementById('display-left');
+var right = document.getElementById('display-right');
 var slideIndex = 1;
 showDivs(slideIndex);
+
+left.addEventListener('click', onClick);
+right.addEventListener('click', onClick);
+
+function onClick(event) {
+  if (event.target === left) {
+    plusDivs(-1);
+  }
+  if (event.target === right) {
+    plusDivs(1);
+  }
+}
 
 function plusDivs(n) {
   showDivs(slideIndex += n);
