1b219ea932a7f8193f1ae13ccec40091ec5273aa
Nikko Pisciotti <t.bird.13@hotmail.com>
2016-11-10 11:06:38 -0800

    reset forms after register

--- js/form.js
+++ js/form.js
@@ -3,6 +3,17 @@ var studentSubjects = [];
 var currentStudent = {};
 var newStudentForm = document.getElementById('newStudentForm');
 
+function resetForm(){
+  var fName = document.getElementById('fNameForm');
+  var lName = document.getElementById('lNameForm');
+  var email = document.getElementById('emailForm');
+  var checkBoxes = document.getElementsByClassName('checkbox');
+  fName.value = '';
+  lName.value = '';
+  email.value = '';
+  checkBoxes.checked = false;
+}
+
 function Student(fName, lName, email, subjects) {
   this.fName = fName;
   this.lName = lName;
@@ -19,12 +30,12 @@ function submitForm(event){
   for(var i = 0; i < checkBoxes.length; i++){
     if(checkBoxes[i].checked === true){
       studentSubjects.push(checkBoxes[i].name);
-      console.log(checkBoxes[i].name);
     }
   }
   var student = new Student(fName, lName, email, studentSubjects);
   currentStudent = student;
   localStorage.setItem('currentUser', JSON.stringify(currentStudent));
+  resetForm();
 }
 
 newStudentForm.addEventListener('submit', submitForm);
